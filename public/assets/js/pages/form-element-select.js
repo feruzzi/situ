45a5b5e79d85fab9b8bff110faf99844b583b753
Feruzzi Graha <feruzzig@gmail.com>
2023-02-24 20:09:50 +0700

    add class del-list for delete icon on item-outs page

--- public/assets/js/pages/form-element-select.js
+++ public/assets/js/pages/form-element-select.js
@@ -1,15 +1,26 @@
-let choices = document.querySelectorAll('.choices');
+let choices = document.querySelectorAll(".choices");
 let initChoice;
-for(let i=0; i<choices.length;i++) {
-  if (choices[i].classList.contains("multiple-remove")) {
-    initChoice = new Choices(choices[i],
-      {
-        delimiter: ',',
-        editItems: true,
-        maxItemCount: -1,
-        removeItemButton: true,
-      });
-  }else{
-    initChoice = new Choices(choices[i]);
-  }
+for (let i = 0; i < choices.length; i++) {
+    if (choices[i].classList.contains("multiple-remove")) {
+        initChoice = new Choices(choices[i], {
+            delimiter: ",",
+            editItems: true,
+            maxItemCount: -1,
+            removeItemButton: true,
+            customProperties: {
+                unit: "data-unit",
+            },
+        });
+    } else {
+        initChoice = new Choices(choices[i], {
+            placeholder: true,
+            placeholderValue: "Cari",
+            searchPlaceholderValue: "Cari...",
+            shouldSort: true,
+            renderSelectedChoices: "auto",
+            customProperties: {
+                "data-unit": "x",
+            },
+        });
+    }
 }
