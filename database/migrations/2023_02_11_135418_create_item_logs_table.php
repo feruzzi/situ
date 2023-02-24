bdd54375d32fa85326b14c47d56af963b338e3d4
Feruzzi Graha <feruzzig@gmail.com>
2023-02-24 20:09:06 +0700

    changes description date_end and guest to nullable and changes log_id to not unique

--- database/migrations/2023_02_11_135418_create_item_logs_table.php
+++ database/migrations/2023_02_11_135418_create_item_logs_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
     {
         Schema::create('item_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('log_id')->unique();
+            $table->string('log_id');
             $table->string('item_id');
             $table->string('description')->nullable();
             $table->integer('qty');
             $table->date('date_start');
-            $table->date('date_end');
+            $table->date('date_end')->nullable();
             $table->string('officer');
-            $table->string('guest');
+            $table->string('guest')->nullable();
             $table->foreign('item_id')->references('item_id')->on('items');
             $table->foreign('officer')->references('username')->on('users');
             $table->string('company_id');
